76bf6c349f8b28dd4ab9942e07131e4438f1ba82
lakshyabatman <lakshya.khera@gmail.com>
2019-02-08 17:43:52 +0530

    Node and Express integrate update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + "/public"));
 app.get("/",(res,req)=>{
     res.render('index.html');
 })
-
-app.listen(4000,()=>{
-    console.log("app is running");
+const port=process.env.PORT || 4000
+app.listen(port,function(){
+    console.log("Server started at Port ",port)
 })
